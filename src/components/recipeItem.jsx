3e4c90c79ed9c0f3f9c895c140b672aa9feaac78
Sanjeet Tripathy <sanjeet.724@gmail.com>
2019-02-23 10:52:59 -0800

    Made the components stateless

--- src/components/recipeItem.jsx
+++ src/components/recipeItem.jsx
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RecipeItem extends Component {
-    state = {
-        checked: false
-    }
-
-    render() {
-        const { recipe, handleCheckboxClick } = this.props;
-        const { name, cook_time, type } = recipe;
-        const mins = cook_time + " mins";
-        return (
-            <div className="list-group-item">
-                <div className="recipe-list-item">
-                    <input
-                        type="checkbox"
-                        onClick={(e) => handleCheckboxClick(recipe, e.target.checked)}
-                        className="check-box"></input>
-                    <h6 className="recipe-name">{name}</h6>
-                    <h6 className="badges">
-                        <span className="badge badge-pill badge-secondary badge-recipe">{type}</span>
-                        <span className="badge badge-pill badge-info">{mins}</span>
-                    </h6>
-                </div>
+const RecipeItem = ({ recipe, handleCheckboxClick }) => {
+    const { name, cook_time, type } = recipe;
+    const mins = cook_time + " mins";
+    return (
+        <div className="list-group-item">
+            <div className="recipe-list-item">
+                <input
+                    type="checkbox"
+                    onClick={(e) => handleCheckboxClick(recipe, e.target.checked)}
+                    className="check-box"></input>
+                <h6 className="recipe-name">{name}</h6>
+                <h6 className="badges">
+                    <span className="badge badge-pill badge-secondary badge-recipe">{type}</span>
+                    <span className="badge badge-pill badge-info">{mins}</span>
+                </h6>
             </div>
-
-        )
-    }
+        </div>
+    );
 }
 
-export default RecipeItem
+export default RecipeItem;
+
--- src/components/recipeList.jsx
+++ src/components/recipeList.jsx
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RecipeItem from './recipeItem';
 
-class RecipeList extends Component {
-
-    state = {
-        selected: []
-    }
-
-    render() {
-        const { recipes, handleCheckboxClick } = this.props;
-        return (
-            <div className="recipe-list">
-                <ul className="list-group">
-                    {recipes.map((recipe, i) =>
-                        <RecipeItem key={i}
-                            recipe={recipe}
-                            handleCheckboxClick={handleCheckboxClick} />
-                    )}
-                </ul>
-            </div>
-        );
-    }
+const RecipeList = ({ recipes, handleCheckboxClick }) => {
+    return (
+        <div className="recipe-list">
+            <ul className="list-group">
+                {recipes.map((recipe, i) =>
+                    <RecipeItem key={i}
+                        recipe={recipe}
+                        handleCheckboxClick={handleCheckboxClick} />
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default RecipeList;
